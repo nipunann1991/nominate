1994e84a9f963425f505f8aedf126c5e972ea926
Nipuna Nanayakkara <Nipuna.Nanayakkara@volume.co.uk>
2021-06-08 14:50:07 +0530

    truncate string added .

--- src/app/app.shared.module.ts
+++ src/app/app.shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core'; 
 import { TimeConverterPipe } from './pipes/time-converter.pipe';
 import { BeautifyTextPipe } from './pipes/beautify-text.pipe';
+import { TruncateTextPipe } from './pipes/truncate-text.pipe';
 
 @NgModule({
   declarations: [
     TimeConverterPipe,
-    BeautifyTextPipe
+    BeautifyTextPipe,
+    TruncateTextPipe
   ],
   imports: [ 
 
   ],
   exports: [  
     TimeConverterPipe,
-    BeautifyTextPipe
+    BeautifyTextPipe,
+    TruncateTextPipe
   ],
 
   entryComponents: []
